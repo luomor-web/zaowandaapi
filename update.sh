eea28ad3c78bf858b012962efefb0c471b9093e5
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-03 10:08:50 +0800

    update

--- update.sh
+++ update.sh
@@ -6,3 +6,6 @@ cp -r docker/lillia/question/today/questionToday.js ../eladmin-web/src/api
 mkdir -p ../eladmin-web/src/views/question/todayAnswer
 cp -r docker/lillia/question/todayAnswer/index.vue ../eladmin-web/src/views/question/todayAnswer
 cp -r docker/lillia/question/todayAnswer/questionTodayAnswer.js ../eladmin-web/src/api
+mkdir -p ../eladmin-web/src/views/question/exam
+cp -r docker/lillia/question/exam/index.vue ../eladmin-web/src/views/question/exam
+cp -r docker/lillia/question/exam/questionTodayAnswer.js ../eladmin-web/src/api
