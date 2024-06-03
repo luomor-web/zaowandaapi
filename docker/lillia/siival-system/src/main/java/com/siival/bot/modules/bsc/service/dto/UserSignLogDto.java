f3b3704cea6a33438aecc6c52470860cac1a60e9
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-03 11:36:29 +0800

    update

---
+++ docker/lillia/siival-system/src/main/java/com/siival/bot/modules/bsc/service/dto/UserSignLogDto.java
@@ -0,0 +1,44 @@
+/*
+*  Copyright 2019-2020 Zheng Jie
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package com.siival.bot.modules.bsc.service.dto;
+
+import lombok.Data;
+import java.sql.Timestamp;
+import java.io.Serializable;
+
+/**
+* @website
+* @description /
+* @author Peter
+* @date 2024-06-03
+**/
+@Data
+public class UserSignLogDto implements Serializable {
+
+    private Integer id;
+
+    /** 用户id */
+    private Integer uid;
+
+    /** 签到获得积分数量用于展示 */
+    private Integer integral;
+
+    /** 创建时间 */
+    private Timestamp createTime;
+
+    /** 更新时间 */
+    private Timestamp updateTime;
+}
