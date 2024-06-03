6bfecd26c898bcb88e09c3d881c97740a91fa9aa
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-03 11:29:57 +0800

    update

---
+++ docker/lillia/views/user/info/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="id">
+            <el-input v-model="form.id" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="用户id" prop="uid">
+            <el-input v-model="form.uid" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="积分">
+            <el-input v-model="form.integral" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="累计签到天数">
+            <el-input v-model="form.signTotalCount" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="连续签到天数">
+            <el-input v-model="form.signContinuousCount" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="创建时间" prop="createTime">
+            <el-input v-model="form.createTime" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="更新时间">
+            <el-input v-model="form.updateTime" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.status.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="id" label="id" />
+        <el-table-column prop="uid" label="用户id" />
+        <el-table-column prop="integral" label="积分" />
+        <el-table-column prop="signTotalCount" label="累计签到天数" />
+        <el-table-column prop="signContinuousCount" label="连续签到天数" />
+        <el-table-column prop="createTime" label="创建时间" />
+        <el-table-column prop="updateTime" label="更新时间" />
+        <el-table-column v-if="checkPer(['admin','userInfo:edit','userInfo:del'])" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudUserInfo from '@/api/userInfo'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+// import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = { id: null, uid: null, integral: null, signTotalCount: null, signContinuousCount: null, createTime: null, updateTime: null }
+export default {
+  name: 'UserInfo',
+  components: { pagination, crudOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  cruds() {
+    return CRUD({ title: '用户信息', url: 'bsc/userInfo', idField: 'id', sort: 'id,desc', crudMethod: { ...crudUserInfo }})
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'userInfo:add'],
+        edit: ['admin', 'userInfo:edit'],
+        del: ['admin', 'userInfo:del']
+      },
+      rules: {
+        uid: [
+          { required: true, message: '用户id不能为空', trigger: 'blur' }
+        ],
+        createTime: [
+          { required: true, message: '创建时间不能为空', trigger: 'blur' }
+        ]
+      }    }
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
