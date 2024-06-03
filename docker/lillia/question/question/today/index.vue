9741756a084183dfccfde80fd9b4e7ccc0b929c4
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-03 09:05:00 +0800

    update

---
+++ docker/lillia/question/question/today/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="app-container">
+    <!--工具栏-->
+    <div class="head-container">
+      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+      <crudOperation :permission="permission" />
+      <!--表单组件-->
+      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+          <el-form-item label="id">
+            <el-input v-model="form.id" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="题目id" prop="qid">
+            <el-input v-model="form.qid" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="获得积分">
+            <el-input v-model="form.integral" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="用户id" prop="uid">
+            <el-input v-model="form.uid" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="用户答案" prop="userAnswer">
+            <el-input v-model="form.userAnswer" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="0-答错, 1-答对">
+            <el-input v-model="form.status" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="创建时间" prop="createTime">
+            <el-input v-model="form.createTime" style="width: 370px;" />
+          </el-form-item>
+          <el-form-item label="更新时间">
+            <el-input v-model="form.updateTime" style="width: 370px;" />
+          </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button type="text" @click="crud.cancelCU">取消</el-button>
+          <el-button :loading="crud.status.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        </div>
+      </el-dialog>
+      <!--表格渲染-->
+      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="id" label="id" />
+        <el-table-column prop="qid" label="题目id" />
+        <el-table-column prop="integral" label="获得积分" />
+        <el-table-column prop="uid" label="用户id" />
+        <el-table-column prop="userAnswer" label="用户答案" />
+        <el-table-column prop="status" label="0-答错, 1-答对" />
+        <el-table-column prop="createTime" label="创建时间" />
+        <el-table-column prop="updateTime" label="更新时间" />
+        <el-table-column v-if="checkPer(['admin','questionTodayAnswer:edit','questionTodayAnswer:del'])" label="操作" width="150px" align="center">
+          <template slot-scope="scope">
+            <udOperation
+              :data="scope.row"
+              :permission="permission"
+            />
+          </template>
+        </el-table-column>
+      </el-table>
+      <!--分页组件-->
+      <pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+import crudQuestionTodayAnswer from '@/api/questionTodayAnswer'
+import CRUD, { presenter, header, form, crud } from '@crud/crud'
+import rrOperation from '@crud/RR.operation'
+import crudOperation from '@crud/CRUD.operation'
+import udOperation from '@crud/UD.operation'
+import pagination from '@crud/Pagination'
+
+const defaultForm = { id: null, qid: null, integral: null, uid: null, userAnswer: null, status: null, createTime: null, updateTime: null }
+export default {
+  name: 'QuestionTodayAnswer',
+  components: { pagination, crudOperation, rrOperation, udOperation },
+  mixins: [presenter(), header(), form(defaultForm), crud()],
+  cruds() {
+    return CRUD({ title: '每日一题答题', url: 'bsc/questionTodayAnswer', idField: 'id', sort: 'id,desc', crudMethod: { ...crudQuestionTodayAnswer }})
+  },
+  data() {
+    return {
+      permission: {
+        add: ['admin', 'questionTodayAnswer:add'],
+        edit: ['admin', 'questionTodayAnswer:edit'],
+        del: ['admin', 'questionTodayAnswer:del']
+      },
+      rules: {
+        qid: [
+          { required: true, message: '题目id不能为空', trigger: 'blur' }
+        ],
+        uid: [
+          { required: true, message: '用户id不能为空', trigger: 'blur' }
+        ],
+        userAnswer: [
+          { required: true, message: '用户答案不能为空', trigger: 'blur' }
+        ],
+        createTime: [
+          { required: true, message: '创建时间不能为空', trigger: 'blur' }
+        ]
+      }    }
+  },
+  methods: {
+    // 钩子：在获取表格数据之前执行，false 则代表不获取数据
+    [CRUD.HOOK.beforeRefresh]() {
+      return true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ docker/lillia/siival-system/src/main/java/com/siival/bot/modules/bsc/domain/QuestionTodayAnswer.java
@@ -0,0 +1,79 @@
+/*
+*  Copyright 2019-2020 Zheng Jie
+*
+*  Licensed under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*  http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/
+package com/siival/bot/modules/bsc.domain;
+
+import lombok.Data;
+import cn.hutool.core.bean.BeanUtil;
+import io.swagger.annotations.ApiModelProperty;
+import cn.hutool.core.bean.copier.CopyOptions;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.sql.Timestamp;
+import java.io.Serializable;
+
+/**
+* @website
+* @description /
+* @author Peter
+* @date 2024-06-03
+**/
+@Entity
+@Data
+@Table(name="question_today_answer")
+public class QuestionTodayAnswer implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    @ApiModelProperty(value = "id")
+    private Integer id;
+
+    @Column(name = "qid",nullable = false)
+    @NotNull
+    @ApiModelProperty(value = "题目id")
+    private Integer qid;
+
+    @Column(name = "integral")
+    @ApiModelProperty(value = "获得积分")
+    private Integer integral;
+
+    @Column(name = "uid",nullable = false)
+    @NotNull
+    @ApiModelProperty(value = "用户id")
+    private Integer uid;
+
+    @Column(name = "user_answer",nullable = false)
+    @NotBlank
+    @ApiModelProperty(value = "用户答案")
+    private String userAnswer;
+
+    @Column(name = "status")
+    @ApiModelProperty(value = "0-答错, 1-答对")
+    private Integer status;
+
+    @Column(name = "create_time",nullable = false)
+    @NotNull
+    @ApiModelProperty(value = "创建时间")
+    private Timestamp createTime;
+
+    @Column(name = "update_time")
+    @ApiModelProperty(value = "更新时间")
+    private Timestamp updateTime;
+
+    public void copy(QuestionTodayAnswer source){
+        BeanUtil.copyProperties(source,this, CopyOptions.create().setIgnoreNullValue(true));
+    }
+}
