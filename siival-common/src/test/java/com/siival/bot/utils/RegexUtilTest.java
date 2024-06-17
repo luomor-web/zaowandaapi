b18a3a728dc021d45e86c8483bfe69b139b35505
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-17 10:29:25 +0800

    update

--- siival-common/src/test/java/com/siival/bot/utils/RegexUtilTest.java
+++ siival-common/src/test/java/com/siival/bot/utils/RegexUtilTest.java
@@ -11,7 +11,7 @@ public class RegexUtilTest {
                 "a)保險人有可能變成\n" + //
                 "資不抵債的危險性 b)潛在利益有關的不確定性\n" + //
                 "c)對可能發生的風險有保護措施 d)潛在損失有關的不確定性";
-        questionAll = "testa)testb)c)testd)test";
+        questionAll = "testa)testb)testc)testd)test";
         List<String> data = RegexUtil.getMatches(RegexUtil.REGEX_QUESTION1, questionAll);
         System.out.println(data.size());
         System.out.println(data);
