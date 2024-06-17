685d56dd6f8273c506560335fecefd25e53a6c3c
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-17 09:45:30 +0800

    update

---
+++ siival-common/src/test/java/com/siival/bot/utils/RegexUtilTest.java
@@ -0,0 +1,17 @@
+package com.siival.bot.utils;
+
+import org.junit.Test;
+
+import java.util.List;
+
+public class RegexUtilTest {
+    @Test
+    public void test1() {
+        String questionAll = "「風險」的定義是：\\n" + //
+                "a)保險人有可能變成\n" + //
+                "資不抵債的危險性 b)潛在利益有關的不確定性\\n" + //
+                "c)對可能發生的風險有保護措施 d)潛在損失有關的不確定性";
+        List<String> data = RegexUtil.getMatches(RegexUtil.REGEX_QUESTION1, questionAll);
+        System.out.println(data);
+    }
+}
