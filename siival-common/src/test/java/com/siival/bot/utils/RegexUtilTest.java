9d4cc096e35280736cb356cb8925b29df4d91273
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-17 10:47:00 +0800

    update

--- siival-common/src/test/java/com/siival/bot/utils/RegexUtilTest.java
+++ siival-common/src/test/java/com/siival/bot/utils/RegexUtilTest.java
@@ -2,7 +2,9 @@ package com.siival.bot.utils;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexUtilTest {
@@ -19,5 +21,21 @@ public class RegexUtilTest {
         String input = "(test)";
         String test = Pattern.quote(input);
         System.out.println(test);
+
+        String str = "select * from order where createdUser = ${currentUser} and  depart = ${currentOrg} and status = 'VALID'";
+        String reg = "\\$\\{[a-zA-Z0-9]+\\}";//定义正则表达式
+ 
+        Pattern patten = Pattern.compile(reg);//编译正则表达式
+        Matcher matcher = patten.matcher(str);// 指定要匹配的字符串
+ 
+        List<String> matchStrs = new ArrayList<>();
+ 
+        while (matcher.find()) { //此处find（）每次被调用后，会偏移到下一个匹配
+           matchStrs.add(matcher.group());//获取当前匹配的值
+        }
+ 
+        for (int i = 0; i < matchStrs.size(); i++) {
+            System.out.println(matchStrs.get(i));
+        }
     }
 }
