906791a1a8a0c45219c690e38e22ca914a091240
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-23 21:35:46 +0800

    update

--- siival-system/src/main/java/com/siival/bot/modules/bsc/service/file/FileBaseService.java
+++ siival-system/src/main/java/com/siival/bot/modules/bsc/service/file/FileBaseService.java
@@ -6,7 +6,7 @@ import com.siival.bot.modules.bsc.service.dto.LilliaFileBatchDto;
 
 public interface FileBaseService {
     public static final int UPLOAD_TYPE_1 = 1;
-    public static final int UPLOAD_TYPE_2 = 2;
+    public static final int UPLOAD_TYPE_FINAL = 2;
 
     public void read(File localFile) throws Exception;
 
--- siival-system/src/main/java/com/siival/bot/modules/bsc/service/file/FileServiceBuild.java
+++ siival-system/src/main/java/com/siival/bot/modules/bsc/service/file/FileServiceBuild.java
@@ -6,8 +6,8 @@ public class FileServiceBuild {
     public static FileBaseService buildService(int uploadType) {
         if(uploadType == FileBaseService.UPLOAD_TYPE_1) {
             return BeanUtil.getBean(ExcelExam1Service.class);
-        } else if(uploadType == FileBaseService.UPLOAD_TYPE_2) {
-            return BeanUtil.getBean(ExcelExam2Service.class);
+        } else if(uploadType == FileBaseService.UPLOAD_TYPE_FINAL) {
+            return BeanUtil.getBean(ExcelExamFinalService.class);
         } else {
             return BeanUtil.getBean(ExcelExam1Service.class);
         }
