da118a5d3612974fc79984cb37cdc8dadc813335
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-27 14:56:07 +0800

    update

--- siival-system/src/test/java/com/siival/bot/Test1.java
+++ siival-system/src/test/java/com/siival/bot/Test1.java
@@ -13,6 +13,7 @@ public class Test1 {
     @Test
     public void testChapter() {
         String chapter = "1.1.1";
+        chapter = "1.1999999999999999555910790149937383830547332763671875";
         String[] chapters = chapter.split("\\.");
         System.out.println(chapters[0]);
     }
