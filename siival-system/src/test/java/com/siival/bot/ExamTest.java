b03e58badd8e1cafde16b4b81db59d4d36f9988a
zhangchunsheng <zhangchunsheng423@gmail.com>
2024-06-27 14:32:52 +0800

    update

--- siival-system/src/test/java/com/siival/bot/ExamTest.java
+++ siival-system/src/test/java/com/siival/bot/ExamTest.java
@@ -24,4 +24,11 @@ public class ExamTest {
     public void testExamTemp() {
         examTempService.handlerExamTemp();
     }
+
+    @Test
+    public void testChapter() {
+        String chapter = "1.1.1";
+        System.out.println(chapter.split("."));
+    }
 }
+
